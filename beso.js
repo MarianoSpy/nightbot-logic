95d70d72d02341e2b0cea63788ebd2c4946991a7
MarianoSpy <138595279+MarianoSpy@users.noreply.github.com>
2026-05-26 16:32:48 -0300

    Add beso.js with random message selection

---
+++ beso.js
@@ -0,0 +1,12 @@
+const user = args[0];
+const target = args[1];
+const roll = Math.floor(Math.random() * 4);
+
+const mensajes = [
+  "mensaje 1"
+  "mensaje 2"
+  "mensaje 3"
+  "mensaje 4
+];
+
+mensajes[roll];
