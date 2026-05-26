ba88430dc2ea475a5d277227a35a9d327b3d2155
MarianoSpy <138595279+MarianoSpy@users.noreply.github.com>
2026-05-26 16:45:11 -0300

    Update beso.js

--- beso.js
+++ beso.js
@@ -3,10 +3,10 @@ const touser = args[1];
 const roll = Math.floor(Math.random() * 4);
 
 const mensajes = [
-  "${user} mensaje 1 ${touser}",
-  "${user} mensaje 2 ${touser}",
-  "${user} mensaje 3 ${touser}",
-  "${user} mensaje 4 ${touser}"
+  `${user} mensaje 1 ${touser}`,
+  `${user} mensaje 2 ${touser}`,
+  `${user} mensaje 3 ${touser}`,
+  `${user} mensaje 4 ${touser}`
 ];
 
 mensajes[roll];
