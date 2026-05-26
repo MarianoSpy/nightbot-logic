553f86d353c06e2ddc4420597794f102e1c25458
MarianoSpy <138595279+MarianoSpy@users.noreply.github.com>
2026-05-26 16:02:29 -0300

    Update abrazo.js

--- abrazo.js
+++ abrazo.js
@@ -1,1 +1,8 @@
+const user = "$user"
+const target = "$touser"
 
+if (user.toLowerCase() === target.toLowerCase()) {
+    `${user} se abrazó a sí mismo... 🥲`
+} else {
+    `Awww, ${user} se abrazó con ${target} 💕`
+}
